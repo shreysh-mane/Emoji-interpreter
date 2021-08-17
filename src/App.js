68f32785832ed236d0917519baf9c9a3ef146481
shreysh-mane <82796751+shreysh-mane@users.noreply.github.com>
2021-08-17 13:54:47 +0530

    style : Layout

--- src/App.js
+++ src/App.js
@@ -8,12 +8,23 @@ export default function App() {
     "😂": "Tears of Joy",
     "😍": "Loved it",
     "🔥": "fire",
-    "😑": "Expression Less"
+    "😑": "Expression Less",
+    "🙃": "Upside down",
+    "😕": "Confused",
+    "👑": "Crown",
+    "🤣": "Laughing Hard",
+    "👍": "Thumbs up"
   };
+
+  let keyData = [];
+  for (let keys of Object.keys(emojiData)) {
+    keyData.push(keys);
+  }
+
   const inputChange = (event) => {
     let input = event.target.value;
     let meaning = emojiData[input];
-    let message = "not in data";
+
     if (input) {
       if (meaning === undefined) {
         meaning = "not in data";
@@ -26,13 +37,18 @@ export default function App() {
 
   return (
     <main>
-      <h2>Find Meaning of Emojis</h2>
-      <input
-        type="text"
-        onChange={inputChange}
-        placeholder="Paste your emoji here"
-      />
-      <h3>{meaning}</h3>
+      <div className="container">
+        <div className="content">
+          <h2>Find Meaning of Emojis</h2>
+          <div className="grid eDisplay">
+            {keyData.map((data) => {
+              return <h3 className="ed">{data}</h3>;
+            })}
+          </div>
+          <input type="text" onChange={inputChange} placeholder="Paste Emoji" />
+          <h3>{meaning}</h3>
+        </div>
+      </div>
     </main>
   );
 }
